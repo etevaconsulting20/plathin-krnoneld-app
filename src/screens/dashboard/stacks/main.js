6d72f21d2a58723da00a6a65a7b07422c1b2d833
Gajashree Rakhe <neha.desai@etevaconsulting.com>
2021-03-18 17:13:51 +0530

    folder implementation changes

--- src/screens/dashboard/stacks/main.js
+++ src/screens/dashboard/stacks/main.js
@@ -1,12 +1,12 @@
-import React, {Component} from 'react';
-import {View, ScrollView, RefreshControl} from 'react-native';
-import {connect} from 'react-redux';
-import {getAllFiles} from '../../../actions/index';
+import React, { Component } from 'react';
+import { View, ScrollView, RefreshControl } from 'react-native';
+import { connect } from 'react-redux';
+import { getAllFiles } from '../../../actions/index';
 import LoadingIndicator from '../../../components/loadingIndicator';
-import {appConfig} from '../../../settings/settings';
-import {Icon, SearchBar} from 'react-native-elements';
-import {filter, includes} from 'lodash';
-import moment, {locales} from 'moment';
+import { appConfig } from '../../../settings/settings';
+import { Icon, SearchBar } from 'react-native-elements';
+import { filter, includes } from 'lodash';
+import moment, { locales } from 'moment';
 import I18n from 'react-native-i18n';
 
 
@@ -24,6 +24,9 @@ import {
 class DashboardMainScreen extends Component {
   state = {
     searchText: '',
+    flag:false,
+    filterArray:[],
+    openId:''
   };
   componentDidMount = () => {
     this.props.getAllFiles();
@@ -32,13 +35,13 @@ class DashboardMainScreen extends Component {
     })
   };
   viewPdf = (name, id) => {
-    this.props.navigation.navigate('dashboard-viewpdf', {id: id, name: name});
+    this.props.navigation.navigate('dashboard-viewpdf', { id: id, name: name });
   };
   refreshPage = () => {
     this.props.getAllFiles();
   };
   onSearch = (text) => {
-    this.setState({searchText: text});
+    this.setState({ searchText: text });
   };
   getFilteredList = (list) => {
     let searchText = this.state.searchText;
@@ -51,73 +54,121 @@ class DashboardMainScreen extends Component {
 
     return result;
   };
+  openFiles=(name,id)=>{
+    const largeGroup = this.props.files&& this.props.files.filter(activity => (
+      activity.folderName ===name
+  )); 
+    
+    this.setState({flag:true,filterArray:largeGroup,openId:id})
+  }
+  
+
   render() {
-    const {loading, files} = this.props;
-    let sortData=this.props.files && this.props.files;
-    let fileData =sortData.sort(function(a,b){
+    const { loading, files } = this.props;
+    let sortData = this.props.files && this.props.files;
+    let fileData = sortData.sort(function (a, b) {
       return new Date(b.sharedDate) - new Date(a.sharedDate);
     });
+    let arr = [];
+    const largeGroup = fileData && fileData.filter(activity => (
+      activity.folderName &&
+      arr.push(activity.folderName)
+    ));
+    let uniqueNames = Array.from(new Set(arr));
+    console.log("largeGroup", this.state.filterArray);
     return (
       <>
         {/* <LoadingIndicator
           isVissible={loading}
           message={'fetching files'}></LoadingIndicator> */}
-          <SearchBar
-                lightTheme
-                placeholder={I18n.t('placeholder_typeHere')}
-                round
-                onChangeText={(text) => this.onSearch(text)}
-                value={this.state.searchText}
-              />
-       
+        <SearchBar
+          lightTheme
+          placeholder={I18n.t('placeholder_typeHere')}
+          round
+          onChangeText={(text) => this.onSearch(text)}
+          value={this.state.searchText}
+        />
+
         <ScrollView
           refreshControl={
             <RefreshControl refreshing={loading} onRefresh={this.refreshPage} />
           }>
           <List>
-            {fileData
-              ? this.getFilteredList(fileData).map((file, index) => {
-                  return (
-                    <ListItem
-                      key={index}
-                      onPress={() => this.viewPdf(file.name, file.id)}>
-                      <Left>
+            {uniqueNames.map((name, key) => {
+              return (
+                <ListItem
+                  key={key}
+                  onPress={()=>this.openFiles(name,key)}
+                >
+                  <Left>
+                    <Text
+                      style={{
+                        fontFamily: appConfig.fontFamily,
+                        color: 'black',
+                      }}>
+                      {name}
+                     
+                    </Text>
+
+                  </Left>
+
+                  <Right>
+                    {/* <Icon
+                      name="download"
+                      type="font-awesome"
+                      color={appConfig.primaryColor}></Icon> */}
+                  </Right>
+                 
+                 
+                </ListItem>
+                )
+            })}
+             {/* {this.state.flag && this.state.openId ===key ? */}
+                  <List>
+            {this.state.filterArray.map((file, index) => {
+                return (
+                  <ListItem
+                    key={index}
+                    onPress={() => this.viewPdf(file.name, file.id)}>
+                    <Left>
+                      <Text
+                        style={{
+                          fontFamily: appConfig.fontFamily,
+                          color: 'black',
+                        }}>
+                        {file.name}
                         <Text
-                          style={{
-                            fontFamily: appConfig.fontFamily,
-                            color: 'black',
-                          }}>
-                          {file.name}
-                          <Text
                           style={{
                             fontFamily: appConfig.fontFamily,
                             color: 'grey',
                           }}>
-                           {`\n ${moment(file.sharedDate).format("DD/MM/YYYY")}`}
+                          {`\n ${moment(file.sharedDate).format("DD/MM/YYYY")}`}
                         </Text>
-                        </Text>
-                        
-                      </Left>
-                     
-                      <Right>
-                        <Icon
-                          name="download"
-                          type="font-awesome"
-                          color={appConfig.primaryColor}></Icon>
-                      </Right>
-                      
-                    </ListItem>
-                  );
-                })
-              : null}
+                      </Text>
+
+                    </Left>
+
+                    <Right>
+                      <Icon
+                        name="download"
+                        type="font-awesome"
+                        color={appConfig.primaryColor}></Icon>
+                    </Right>
+
+                  </ListItem>
+                );
+              })}
+              
           </List>
+          </List>
+          
         </ScrollView>
       </>
     );
   }
 }
 
-const mapStateToProps = ({files}) => {
+const mapStateToProps = ({ files }) => {
   return files;
 };
 
